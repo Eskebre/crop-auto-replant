017f5d86dba5914ab11f36d6c327c611a1422633
Eskebre <ethan05@outlook.com.au>
2025-02-08 19:32:33 +1100

    Added hoeing to clear 3x3 area

--- src/main/java/eskebre/crop/CropAutoReplant.java
+++ src/main/java/eskebre/crop/CropAutoReplant.java
@@ -4,14 +4,17 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+
 import net.minecraft.block.CropBlock;
+import net.minecraft.item.ItemStack;
+
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
+
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,15 +38,53 @@ public class CropAutoReplant implements ModInitializer {
 		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
 			BlockPos pos = hitResult.getBlockPos();
 			BlockState state = world.getBlockState(pos);
+			
+			//Client Code
+			if (world.isClient) {
+				if (state.getBlock() instanceof CropBlock cropBlock) {
+					int MAX_AGE = cropBlock.getMaxAge();
+					int CURRENT_AGE = cropBlock.getAge(state);
+					if (CURRENT_AGE == MAX_AGE) player.swingHand(hand.MAIN_HAND);
+				}
+				return ActionResult.PASS;
+			}
+			
+			//Server Code
 			if (state.getBlock() instanceof CropBlock cropBlock) {
 				int MAX_AGE = cropBlock.getMaxAge();
 				int CURRENT_AGE = cropBlock.getAge(state);
-				LOGGER.info(String.valueOf(MAX_AGE));
-				if (CURRENT_AGE == MAX_AGE) {
+				ItemStack handItem = player.getStackInHand(hand.MAIN_HAND);
+				//3x3 Hoe Break
+				if (handItem.isIn(ItemTags.HOES)) {
+					player.swingHand(hand);
+					for (int x = -1; x <=1; x++) {
+						for (int z = -1; z <= 1; z++) {
+							BlockPos iPos = pos.add(x, 0, z);
+							BlockState iState = world.getBlockState(iPos);
+							if (iState.getBlock() instanceof CropBlock iCropBlock) {
+								int iMAX_AGE = iCropBlock.getMaxAge();
+								int iCURRENT_AGE = iCropBlock.getAge(iState);
+								if (iMAX_AGE == iCURRENT_AGE) {
+									world.setBlockState(iPos, iState.getBlock().getDefaultState(), 3);
+									Block.dropStacks(iState, world, iPos);
+									player.swingHand(hand);
+									world.playSound(null, iPos, SoundEvents.BLOCK_CROP_BREAK, SoundCategory.BLOCKS);
+								}
+							}
+						}
+					}
+					
+					return ActionResult.PASS_TO_DEFAULT_BLOCK_ACTION;
+				} 
+				//Single block
+				else if (CURRENT_AGE == MAX_AGE) {
+					
 					world.setBlockState(pos, state.getBlock().getDefaultState());
 					Block.dropStacks(state, world, pos);
-					player.swingHand(hand);
 					world.playSound(null, pos, SoundEvents.BLOCK_CROP_BREAK, SoundCategory.BLOCKS);
+					player.swingHand(hand.MAIN_HAND);
+					
+					
 					return ActionResult.PASS_TO_DEFAULT_BLOCK_ACTION;
 				}
 			}	
